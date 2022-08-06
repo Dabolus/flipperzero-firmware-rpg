42d5c8da604be623489e679ff67c8731174fb522
Giorgio Garasto <giorgio@garasto.me>
2022-08-07 00:19:51 +0200

    fix: decimals lost in division

--- applications/dolphin/helpers/dolphin_state.h
+++ applications/dolphin/helpers/dolphin_state.h
@@ -9,9 +9,11 @@
 #define REQUIRED_XP(LEVEL)                                            \
     (((LEVEL * LEVEL) + LEVEL) / 2 * LEVEL_XP_REQUIREMENT_INCREMENT - \
      (LEVEL * LEVEL_XP_REQUIREMENT_INCREMENT))
-#define LEVEL_FOR_XP_DIVIDEND(XP)                                                               \
-    (sqrt(LEVEL_XP_REQUIREMENT_INCREMENT * ((2 * XP) + (LEVEL_XP_REQUIREMENT_INCREMENT / 4))) + \
-     (LEVEL_XP_REQUIREMENT_INCREMENT / 2))
+#define LEVEL_FOR_XP_DIVIDEND(XP)                                     \
+    (sqrt(                                                            \
+         LEVEL_XP_REQUIREMENT_INCREMENT *                             \
+         ((2 * XP) + ((double)LEVEL_XP_REQUIREMENT_INCREMENT / 4))) + \
+     ((double)LEVEL_XP_REQUIREMENT_INCREMENT / 2))
 
 #define DOLPHIN_STATE_PATH INT_PATH(DOLPHIN_STATE_FILE_NAME)
 #define DOLPHIN_STATE_HEADER_MAGIC 0xD0
